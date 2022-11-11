85c58dab7d25653deb56659ca2457f7f6ff13550
Alice <aliceerror380@gmail.com>
2022-11-11 20:43:11 +0600

    c# learning switch operator

---
+++ Use_The_switch_operator.cs
@@ -0,0 +1,21 @@
+int dayOfWeek = 5;
+switch (dayOfWeek)
+{
+    case 1:
+    MessageBox.Show("Monday");
+    break;
+    case 2:
+    MessageBox.Show("Tuesday");
+    case 3:
+    MessageBox.Show("Wednesday");
+    case 4:
+    MessageBox.Show("Thursday");
+    case 5:
+    MessageBox.Show("Friday");
+    case 6:
+    MessageBox.Show("Sunday");
+    case 7:
+    MessageBox.Show("Saturday");
+    default:
+    MessageBox.Show("invalid value");
+}
